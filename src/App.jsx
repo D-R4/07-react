8740bda035927c0320bd88835a39c48b419100aa
D-R4 <ibrahimtsuev1@mail.ru>
2023-01-03 21:30:22 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   const [data, setData] = useState([])
 
+  const arr = data.filter(item => item.completed === true)
+  const arr2 = data.filter(item => item.completed === false)
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then((response) =>{
@@ -18,6 +21,9 @@ function App() {
       })
   }, [])
 
+  const completed = (id) => {
+    setData(data.map(item => item.id === id ? {...item, completed: !item.completed} : {...item}))
+  }
 
 
   const handleDelete = (id) =>{
@@ -28,12 +34,17 @@ function App() {
   
   
   return (
-    <ul className="list-group w-50 m-auto">
+    <div>
+      <h1>Всего задач : {data.length}</h1>
+      <h2>Выполнено : {arr.length}</h2>
+      <h2>Не выполнено : {arr2.length}</h2>
+
+      <ul className="list-group w-50 m-auto">
       {data.map(item =>{
         return(
           <li className="list-group-item d-flex justify-content-between" key={item.id}>
               <div>
-                  <input type="checkbox" checked={item.completed} />
+                  <input type="checkbox" checked={item.completed} onChange={() => completed(item.id)} />
               </div>
               {item.title}
 
@@ -45,6 +56,9 @@ function App() {
         )
       })}
     </ul>
+    </div>
+
+    
   );
 }
 
